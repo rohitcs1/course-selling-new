3134e62af3298524032567a220f0dcf2debf609a
Rohit Kumar <rohitcs175@gmail.com>
2026-05-21 11:01:23 +0000

    fix terms page final result

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -3,7 +3,7 @@ export default function TermsPage() {
 		<main className="min-h-screen bg-white">
 			<header className="bg-[#072045] py-12">
 				<div className="mx-auto max-w-6xl px-6 text-left">
-					<h1 className="text-4xl md:text-5xl font-extrabold text-white">Terms of Service</h1>
+					<h1 className="text-4xl md:text-5xl font-extrabold text-white">TERMS OF SERVICE</h1>
 				</div>
 			</header>
 
@@ -15,59 +15,59 @@ export default function TermsPage() {
 				<div>
 					<h3 className="mb-2 text-xl font-bold">1. Terms</h3>
 					<p>
-						By accessing this Website, accessible from https://kabirmehra.in/, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.
+						By accessing this Website, accessible from https://course.elneb.in/, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.
 					</p>
 				</div>
 
 				<div>
 					<h3 className="mb-2 text-xl font-bold">2. Use License</h3>
 					<p>
-						Permission is granted to temporarily download one copy of the materials on kabirmehra.in Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+						Permission is granted to temporarily download one copy of the materials on course.elneb.in Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
 					</p>
 					<ul className="list-disc space-y-2 pl-6">
 						<li>modify or copy the materials;</li>
 						<li>use the materials for any commercial purpose or for any public display;</li>
-						<li>attempt to reverse engineer any software contained on kabirmehra.in's Website;</li>
+						<li>attempt to reverse engineer any software contained on course.elneb.in's Website;</li>
 						<li>remove any copyright or other proprietary notations from the materials; or</li>
-						<li>transferring the materials to another person or "mirror" the materials on any other server.</li>
+						<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
 					</ul>
 					<p>
-						This will let Mehra Enterprises to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format.
+						This will let ELNEB EdTech to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format.
 					</p>
 				</div>
 
 				<div>
 					<h3 className="mb-2 text-xl font-bold">3. Disclaimer</h3>
 					<p>
-						All the materials on Mehra Enterprises's Website are provided "as is". Mehra Enterprises makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, Mehra Enterprises does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.
+						All the materials on ELNEB EdTech's Website are provided "as is". ELNEB EdTech makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, ELNEB EdTech does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.
 					</p>
 				</div>
 
 				<div>
 					<h3 className="mb-2 text-xl font-bold">4. Limitations</h3>
 					<p>
-						Mehra Enterprises or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on Mehra Enterprises's Website, even if Mehra Enterprises or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.
+						ELNEB EdTech or its suppliers will not be held accountable for any damages that will arise with the use or inability to use the materials on ELNEB EdTech's Website, even if ELNEB EdTech or an authorized representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.
 					</p>
 				</div>
 
 				<div>
 					<h3 className="mb-2 text-xl font-bold">5. Revisions and Errata</h3>
 					<p>
-						The materials appearing on Mehra Enterprises's Website may include technical, typographical, or photographic errors. Mehra Enterprises will not promise that any of the materials in this Website are accurate, complete, or current. Mehra Enterprises may change the materials contained on its Website at any time without notice. Mehra Enterprises does not make any commitment to update the materials.
+						The materials appearing on ELNEB EdTech's Website may include technical, typographical, or photographic errors. ELNEB EdTech will not promise that any of the materials in this Website are accurate, complete, or current. ELNEB EdTech may change the materials contained on its Website at any time without notice. ELNEB EdTech does not make any commitment to update the materials.
 					</p>
 				</div>
 
 				<div>
 					<h3 className="mb-2 text-xl font-bold">6. Links</h3>
 					<p>
-						Mehra Enterprises has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by Mehra Enterprises of the site. The use of any linked website is at the user's own risk.
+						ELNEB EdTech has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by ELNEB EdTech of the site. The use of any linked website is at the user's own risk.
 					</p>
 				</div>
 
 				<div>
 					<h3 className="mb-2 text-xl font-bold">7. Site Terms of Use Modifications</h3>
 					<p>
-						Mehra Enterprises may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.
+						ELNEB EdTech may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.
 					</p>
 				</div>
 
@@ -79,7 +79,7 @@ export default function TermsPage() {
 				<div>
 					<h3 className="mb-2 text-xl font-bold">9. Governing Law</h3>
 					<p>
-						Any claim related to Mehra Enterprises's Website shall be governed by the laws of in without regards to its conflict of law provisions.
+						Any claim related to ELNEB EdTech's Website shall be governed by the laws of India without regard to its conflict of law provisions.
 					</p>
 				</div>
 			</section>
@@ -87,8 +87,8 @@ export default function TermsPage() {
 	</section>
 
 			<footer className="mt-12 border-t border-slate-200 pt-6 text-sm text-slate-700 text-center">
-				<div className="mb-2">Term of Service | Privacy Policy | Disclaimer</div>
-				<div>Copyright 2024 - Kabir Mehra - All Rights Reserved</div>
+				<div className="mb-2">TERMS OF SERVICE | Privacy Policy | Disclaimer</div>
+				<div>Copyright 2026 - ELNEB - All Rights Reserved</div>
 			</footer>
 		</main>
 	)
